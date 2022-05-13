ee8cffe41c078d32ef9a02090c76a062ca58cd8d
Priyanka7011 <pk3201134@gmail.com>
2022-05-13 23:47:02 +0530

    create github repositories model

--- recommender/github/models.py
+++ recommender/github/models.py
@@ -11,3 +11,18 @@ class GithubUsers(models.Model):
     total_repos=models.IntegerField(default=0)
     all_repos=models.TextField(max_length=5000,blank=True)
     orgs=models.TextField(max_length=5000,blank=True)
+
+class GithubRepos(models.Model):
+    full_name=models.CharField(max_length=500,blank=True)
+    owner=models.CharField(max_length=500,blank=True)
+    github_url=models.URLField(max_length=500,blank=True)
+    homepage=models.URLField(max_length=500,blank=True)
+    created_at=models.DateTimeField(max_length=500,blank=True)
+    fork_counts=models.IntegerField(default=0)
+    open_issues=models.IntegerField(default=0)
+    stargazers_count=models.IntegerField(default=0)
+    watchers=models.IntegerField(default=0)
+    description=models.TextField(max_length=1000,blank=True)
+    topics=models.TextField(max_length=1000,blank=True)
+    languages=models.TextField(max_length=1000,blank=True)
+    content=models.TextField(max_length=1000,blank=True)
