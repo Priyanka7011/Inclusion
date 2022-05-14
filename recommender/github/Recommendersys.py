eb912e4a574ad020eb4de326f9f1b827c774d083
Aditi-Roy-coder <aditiroy.21.ar@gmail.com>
2022-05-14 19:57:36 +0530

    recommendation - fit repositories

---
+++ recommender/github/Recommendersys.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from .models import *
+import gensim
+from gensim.parsing.preprocessing import preprocess_documents
+
+df=pd.DataFrame((list(GithubRepos.objects.all().values())))
+def fit_repos(keywords):
+  df['description'].fillna('').astype(str)
+  df['content']=df['content'].map(str)
+
+  print(df['content'])
+
+  text_corpus = df['content'].values
+
+  processed_corpus = preprocess_documents(text_corpus)
+  dictionary = gensim.corpora.Dictionary(processed_corpus)
+  bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
+
+  tfidf = gensim.models.TfidfModel(bow_corpus)
+  corpus_tfidf = tfidf[bow_corpus]
+
+  lsi = gensim.models.LsiModel(corpus_tfidf, num_topics=200)
+  index = gensim.similarities.MatrixSimilarity(lsi[corpus_tfidf])
+  #cv = CountVectorizer()
+
+  #count_matrix = cv.fit_transform(keywords)
+
+  # print(count_matrix.toarray())
+
+  #cosine_sim = cosine_similarity(count_matrix) 
+  #return cosine_sim
+  return dictionary,tfidf,index,lsi
