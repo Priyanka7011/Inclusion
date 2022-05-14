5cb9e927661b0fa8f3d140f1d18c408fc44142a8
Priyanka7011 <pk3201134@gmail.com>
2022-05-14 13:10:02 +0530

    populating repo and user database

--- recommender/github/views.py
+++ recommender/github/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
+from .models import *
 # Create your views here.
 def index(request):
     return render(request,'index.html')
