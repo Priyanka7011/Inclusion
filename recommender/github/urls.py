7de228acd83a85a808cecff8a200051c35dac7bb
jishu-yadav <jishuyadavcute88@gmail.com>
2022-05-14 09:47:54 +0530

    url settings

--- recommender/github/urls.py
+++ recommender/github/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('',views.index,name='homepage'),
-    
+    path("", views.index, name="homepage"),
+    path("form", views.form_res, name="form_res"),
+    path("contriform", views.form_contributors_res, name="form_contributors_res"),
 ]
